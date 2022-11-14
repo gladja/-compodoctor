0b79c892ff5c26586203d0ff63e2ebff7d10b9ba
gladja <gladja@yandex.ru>
2022-11-14 18:45:52 +0200

    creadet components

---
+++ src/v-table.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="v-table">
+  <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-table',
+  data() {
+    return {
+      title: 'Hello'
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
